d368904312d712514b563dd00c8af376fe1e29c9
Frank Houweling <houweling.frank@gmail.com>
2014-12-11 14:55:37 +0100

    Relevant image selection

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ ini_set('display_errors', 1);
 
 require_once __DIR__.'/vendor/autoload.php';
 
+use Symfony\Component\CssSelector\CssSelector;
+
 $app = new Silex\Application();
 
 // Template Engine
@@ -29,6 +31,9 @@ $app->register(new Silex\Provider\SessionServiceProvider());
 
 // URLS
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+// Enable our CSS Selector lib
+CssSelector::enableHtmlExtension();
 
 /*
  * Application
@@ -59,6 +64,61 @@ $app->get('/choosecategories', function() use ($app){
     ));
 
 })->bind("choosecategories");
+
+$app->get('/getdata/{city}', function($city) use($app){
+    $city = urlencode($city); // Make names with spaces suitable to be URL arguments
+
+    if( file_exists(__DIR__ . "/data/" . $city . ".json") ){
+        $resultData = file_get_contents(__DIR__ . "/data/" . $city . ".json");
+    }
+    else{
+        $data = file_get_contents( "http://www.geheugenvannederland.nl/?/nl/zoekresultaten/pagina/1/" .  $city . "/%28type%20any%20%22image%20Image%20StillImage%20video%20MovingImage%20audio%20Sound%20text%20Text%22%29%20and%20%28location%20%3D%20%22" .  $city . "%22%29/&colcount=0&wst=" );
+        $data2 =  file_get_contents( "http://www.geheugenvannederland.nl/?/nl/zoekresultaten/pagina/2/" .  $city . "/%28type%20any%20%22image%20Image%20StillImage%20video%20MovingImage%20audio%20Sound%20text%20Text%22%29%20and%20%28location%20%3D%20%22" .  $city . "%22%29/&colcount=0&wst=" );
+        $data3 =  file_get_contents( "http://www.geheugenvannederland.nl/?/nl/zoekresultaten/pagina/3/" .  $city . "/%28type%20any%20%22image%20Image%20StillImage%20video%20MovingImage%20audio%20Sound%20text%20Text%22%29%20and%20%28location%20%3D%20%22" .  $city . "%22%29/&colcount=0&wst=" );
+        $data4 =  file_get_contents( "http://www.geheugenvannederland.nl/?/nl/zoekresultaten/pagina/4/" .  $city . "/%28type%20any%20%22image%20Image%20StillImage%20video%20MovingImage%20audio%20Sound%20text%20Text%22%29%20and%20%28location%20%3D%20%22" .  $city . "%22%29/&colcount=0&wst=" );
+
+
+        // Clean HTML For Page 1
+        $config = HTMLPurifier_Config::createDefault();
+        $purifier = new HTMLPurifier($config);
+        $clean_html = $purifier->purify($data);
+        $clean_html2 = $purifier->purify($data2);
+        $clean_html3 = $purifier->purify($data3);
+        $clean_html4 = $purifier->purify($data4);
+
+        // Select images
+        $simplexml = simplexml_load_string( "<html><body>" . $clean_html . $clean_html2 . $clean_html3 . $clean_html4 . "</body></html>" );
+        $xpathQuery = CssSelector::toXPath(".resultslist img");
+        $images = $simplexml->xpath($xpathQuery);
+
+        // Make array
+        $imgAr = array();
+        foreach( $images as $img ){
+            $response = array();
+            foreach( $img->attributes() as $key => $val ){
+                $response[$key] = (string)$val;
+            }
+            $imgAr[] = $response;
+        }
+
+        // Set the image size to large
+        $i = 0;
+        foreach( $imgAr as $img ){
+            $imgAr[$i]['src'] = str_replace("&role=thumbnail", "&role=image&size=large", $img['src']);
+
+            // Not all images have a 'large' size
+            // I have no idea yet to check which sizes are available, so for now I just
+            // send them all back
+
+            $i++;
+        }
+
+        $resultData = json_encode($imgAr);
+        file_put_contents(__DIR__ . "/data/" . $city . ".json", $resultData);
+    }
+
+    return $resultData;
+});
 
 $app['debug'] = true;
 
--- web/application.js
+++ web/application.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
+    // Select Categories
 
+    if( $('.select-categories') ){
+        // Get data
+        var city = $('.select-categories').attr('data-city');
+
+        $.get('getdata/' + city, function(result){
+            $('.resultsform').removeClass('hidden');
+
+            var items = jQuery.parseJSON(result);
+
+            $('.loading').hide();
+
+            $.each(items, function(key, item){
+                $("ul.results").append('<li><label for="' + item['src'] + '"><img src="' + item['src'] + '" onerror="$(this).parent().parent().hide();" /><p>' +
+                    item['alt'] + '</p></label><input type="checkbox" name="photos[]" id="' + item['src'] + '" value="' + item['src'] + '" /></li>');
+            });
+        });
+
+        $(document).on('change', '.select-categories li input', function(){
+            if( $(this).is(":checked") ){
+                $(this).parent().addClass('selected');
+            } else{
+                $(this).parent().removeClass('selected');
+            }
+        });
+    }
 });
